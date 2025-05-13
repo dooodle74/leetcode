9bb2374d070fafda1f9b07bbdde0232c27ca582e
dooodle74 <dpwang01@gmail.com>
2025-05-13 16:27:56 -0400

    optional 424

--- p229/main.cpp
+++ p229/main.cpp
@@ -10,45 +10,59 @@ int main() {
     vector<int> nums = {2,1,1,3,1,4,5,6};
 
     int threshold = nums.size()/3;
-        int ans1 {0};
-        int ans2 {0};
-        int count1 {0};
-        int count2 {0};
-
-        for (int x : nums) {
-            if (x == ans1){
-                count1++;
-            } else if (x == ans2) {
-                count2++;
-            } else if (count1 == 0){
-                ans1 = x;
-                count1 = 1;
-            } else if (count2 == 0){
-                ans2 = x;
-                count2 = 1;
-            } else {
-                count1--;
-                count2--;
-            }
-        }
-        cout << "ans1: " << ans1 << endl;
-        cout << "ans2: " << ans2 << endl;
-
-        count1 = 0;
-        count2 = 0;
+        int negative_threshold = -static_cast<int>(nums.size()) / 3; // stupid ahh size_t behavior
+        // ans1 loop
+        int ans1;
+        int count {0};
         for (int x : nums){
             if (x == ans1){
-                count1++;
+                count++;
+            } else if (count <= negative_threshold){
+                ans1 = x;
+                count = 1;
+            } else {
+                count--;
+            }
+        }
+
+        // ans2 loop
+        int ans2;
+        count = 0;
+        for (int x : nums){
+            if (x == ans1){
+                continue;
+            }
+            if (count == 0){
+                ans2 = x;
+                count = 1;
+            } else if (x == ans2){
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        cout << "ans1: " << ans1 << endl;
+        cout << "ans2: " << ans2 << endl;
+        
+        // verify
+        int num1_total {0};
+        int num2_total {0};
+        for (int x : nums){
+            if (x == ans1){
+                num1_total++;
             } else if (x == ans2) {
-                count2++;
+                num2_total++;
             }
         }
         vector<int> ans;
-        if (count1 > threshold) {
+        if (num1_total > threshold) {
             ans.push_back(ans1);
         }
-        if (count2 > threshold) {
+        if (num2_total > threshold) {
             ans.push_back(ans2);
         }
+
+        return 0;
 
 }
--- p424/main.cpp
+++ p424/main.cpp
@@ -1,40 +1,37 @@
 #include <vector>
+#include <map>
 #include <iostream>
 using namespace std;
 
 int main(){
     string s = "AABABBA";
-    int k = 1;
-    
+    int k=1;
     if (s.size() < 2){
         return 1;
     }
-
-    vector<char> alphabet = {'A', 'B', 'C', 'D', 'E', 
-    'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 
-    'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+    // init a map of all chars, dynamically update and determine max in each window
+    unordered_map<char, int> counts;
+    // know which letter is most frequent - just keep track of count
+    // no need keep track replaced count - just check if valid for each window
 
     int maxLength {0};
-    
-    for (char target : alphabet) {
-        int left {0};
-        int replaced {0};
+    int left {0};
+    int maxFrequency {0}; // do not care which one
 
-        for (int right = 0; right < s.size(); right++){
-            if (s[right] != target){
-                replaced++;
-            }
-            while (replaced > k){
-                // check current left first...
-                if (s[left] != target){
-                    replaced--;
-                }
-                // ..before going to next 
-                left++;
-            }
-            maxLength = max(maxLength, right-left+1);
+    for (int right = 0; right < s.size(); right++){
+        // loop does the left shift - update current target
+        counts[s[right]]++;
+        maxFrequency = max(maxFrequency, counts[s[right]]);
+        
+        // valid window iff size - maxFreq <= k
+        // we do not care about updating max-frequency (it cannot go higher)
+        // take care of updating in next loop iter
+        while (right-left+1 - maxFrequency > k){
+            counts[s[left]]--;
+            left++;
         }
-        // this letter done
+        maxLength = max(maxLength, right-left+1);
     }
+
     return maxLength;
 }
