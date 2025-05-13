185c25dd842a5eeab3fd431c6981db4961732673
dooodle74 <dpwang01@gmail.com>
2025-05-13 00:22:29 -0400

    229 workinng solution (method 2)

--- p229/main.cpp
+++ p229/main.cpp
@@ -9,77 +9,46 @@ int main() {
     // vector<int> nums = {1,2,1,2,2,3,3,1};  
     vector<int> nums = {2,1,1,3,1,4,5,6};
 
-    // ans1 loop
-    int ans1 = nums[0];
-    // int max_height {0};
-    int count {0};
-    int iters {0};
-    for (int x : nums){
-        cout << "iters: " << iters++ << endl;
-        cout << "x: " << x << endl;
-        cout << "count: " << count << endl;
+    int threshold = nums.size()/3;
+        int ans1 {0};
+        int ans2 {0};
+        int count1 {0};
+        int count2 {0};
+
+        for (int x : nums) {
+            if (x == ans1){
+                count1++;
+            } else if (x == ans2) {
+                count2++;
+            } else if (count1 == 0){
+                ans1 = x;
+                count1 = 1;
+            } else if (count2 == 0){
+                ans2 = x;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
         cout << "ans1: " << ans1 << endl;
-        if (count <= -static_cast<int>(nums.size()) / 3){
-            cout << "if 1" << endl;
-            ans1 = x;
-            count = -static_cast<int>(nums.size()) / 3 ;
-        } else if (x == ans1){
-            cout << "if 2" << endl;
-            count++;
-        } else {
-            cout << "if 3" << endl;
-            count--;
-        }
-        cout << "new ans1: " << ans1 << endl;
-        cout << "new count: " << count << endl;
-        cout << "------------------------" << endl;
-    }
+        cout << "ans2: " << ans2 << endl;
 
-    cout << "ans1: " << ans1 << endl;
-
-    // ans2 loop
-    int ans2;
-    count = 0;
-    for (int x : nums){
-        if (x == ans1){
-            continue;
+        count1 = 0;
+        count2 = 0;
+        for (int x : nums){
+            if (x == ans1){
+                count1++;
+            } else if (x == ans2) {
+                count2++;
+            }
         }
-        if (count == 0){
-            ans2 = x;
-            count = 1;
-        } else if (x == ans2){
-            count++;
-        } else {
-            count--;
+        vector<int> ans;
+        if (count1 > threshold) {
+            ans.push_back(ans1);
         }
-    }
-
-    cout << "ans2: " << ans2 << endl;
-    
-    // verify
-    int num1_total {0};
-    int num2_total {0};
-    for (int x : nums){
-        if (x == ans1){
-            num1_total++;
-        } else if (x == ans2) {
-            num2_total++;
+        if (count2 > threshold) {
+            ans.push_back(ans2);
         }
-    }
-    cout << "num1_total: " << num1_total << endl;
-    cout << "num2_total: " << num2_total << endl;
-
-    vector<int> ans;
-    if (num1_total > nums.size()/3) {
-        ans.push_back(ans1);
-    }
-    if (num2_total > nums.size()/3) {
-        ans.push_back(ans2);
-    }
-    cout << "ans: ";
-    for (int x : ans){
-        cout << x << " ";
-    }
-    cout << endl;
 
 }
