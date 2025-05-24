9eb873ca00ffccefd2ef4e5d30050014856a6adf
dooodle74 <dpwang01@gmail.com>
2025-05-23 23:19:37 -0400

    zero padding problem nums

---
+++ p0567/main.cpp
@@ -0,0 +1,10 @@
+
+#include <vector>
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int main(){
+    // vector: s1_counts[0] = number of 'a' in s1
+    vector<int> s1_counts(26, 0);
+}
