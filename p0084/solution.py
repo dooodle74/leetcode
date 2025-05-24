3cae736b66f236cc4580080efd52624d1a7d3858
dooodle74 <dpwang01@gmail.com>
2025-05-23 23:48:25 -0400

    84 - fixed above, but not the final remainder pass, where left is not considered

--- p0084/solution.py
+++ p0084/solution.py
@@ -6,25 +6,26 @@ class Solution(object):
         """
         # observation: in forward-pass stack, we already know the left-limit 
         # in increasing sequence, current is precisely the left limit
-        # upon decrease, count number of pops made to determine left limit
+        # upon decrease, use index at top of stack to determine left limit
 
         n = len(heights)
         stack = []
         max_area = 0
 
         for i in range(n):
-            count_pops = 0
             while stack and heights[stack[-1]] > heights[i]:
                 j = stack.pop()
-                count_pops += 1
                 # area starting including j expanding to i, maximum height j
                 max_area = max(max_area, (i-j) * heights[j])
             # branch taken: append current increasing after all higher has popped
             # branch not taken: append increasing seq as usual
             stack.append(i)
-            # update max given left limit by counting left-pops until smaller
+            # update max given left limit from top of stack (first (leftmost) smaller element)
             # add 1 to include this as bar as well
-            max_area = max(max_area, (count_pops + 1) * heights[i])
+            left_idx = 0
+            if stack:
+                left_idx = stack[-1]
+            max_area = max(max_area, (i - left_idx + 1) * heights[i])
 
         # end loop - save additional results if final sequence is increasing (unfinished stack)
         while stack:
