d9af9a6e6c1bc0d88882d64c7516aa436fc7778c
dooodle74 <dpwang01@gmail.com>
2025-05-27 19:52:42 -0400

    cleaner code - more defined methods

--- p0146/LRU.cpp
+++ p0146/LRU.cpp
@@ -17,93 +17,88 @@ class CacheBlock {
             this->older = nullptr;
             this->newer = nullptr;
         }
-    };
+};
     
-    class LRUCache {
+class LRUCache {
     private:
         int capacity;
         int currentItems; // count
         unordered_map<int, CacheBlock*> cache; // map for constant access
-        CacheBlock* leftLimit; // two dummy nodes - older than oldest
-        CacheBlock* rightLimit; // and newer than newest
+        CacheBlock* oldDummy; // two dummy nodes - older than oldest
+        CacheBlock* newDummy; // and newer than newest
+
+        // remove a block from the linked list
+        void removeFromLinkedList(CacheBlock* block) {
+            CacheBlock* olderBlock = block->older;
+            CacheBlock* newerBlock = block->newer;
+            olderBlock->newer = newerBlock;
+            newerBlock->older = olderBlock;
+        }
+
+        // insert a block at the end of the linked list (newest)
+        void insertInLinkedList(CacheBlock* block) {
+            CacheBlock* prevNewest = newDummy->older;
+            prevNewest->newer = block;
+            block->older = prevNewest;
+            block->newer = newDummy;
+            newDummy->older = block;
+        }
+        
+        // move a block to the end of the linked list (remove and insert)
+        void moveToNewest(CacheBlock* block) {
+            removeFromLinkedList(block);
+            insertInLinkedList(block);
+        }
+
+        // completely remove a block: from the linked list, hashmap and free memory
+        void removeBlock(CacheBlock* block) {
+            removeFromLinkedList(block);
+            cache.erase(block->key);
+            delete block;
+            currentItems--;
+        }
+
+        // add a completedly new block to the cache (insert in linked list and hashmap)
+        void addNewBlock(int key, int value) {
+            CacheBlock* newCache = new CacheBlock(key, value);
+            insertInLinkedList(newCache);
+            cache[key] = newCache;
+            currentItems++;
+        }
     
     public:
         LRUCache(int capacity) {
             this->capacity = capacity;
             this->currentItems = 0;
             cache.clear();
-            this->leftLimit = new CacheBlock(-1, 0);
-            this->rightLimit = new CacheBlock(-1, 0);
-            leftLimit->newer = rightLimit;
-            rightLimit->older = leftLimit;
+            this->oldDummy = new CacheBlock(-1, 0);
+            this->newDummy = new CacheBlock(-1, 0);
+            oldDummy->newer = newDummy;
+            newDummy->older = oldDummy;
         }
         
         int get(int key) {
             if (cache.find(key) == cache.end()) return -1; // DNE
-            CacheBlock* ans = cache[key];
-    
-            // this becomes most recently accessed - remove from current LL position
-            CacheBlock* olderBlock = ans->older;
-            CacheBlock* newerBlock = ans->newer;
-            olderBlock->newer = newerBlock;
-            newerBlock->older = olderBlock;
-    
-            // after remove, place at current newest
-            CacheBlock* prevNewest = rightLimit->older;
-            prevNewest->newer = ans;
-            ans->older = prevNewest;
-            ans->newer = rightLimit;
-            rightLimit->older = ans;
-    
-            // return value
-            return ans->value;
+            CacheBlock* ansBlock = cache[key];
+            moveToNewest(ansBlock);
+            return ansBlock->value;
         }
         
         void put(int key, int value) {
+            // if the key already exists, update it and move to newest
             if (cache.find(key) != cache.end()){
                 CacheBlock* currCache = cache[key];
-                // already exists, remove from linked list
-                CacheBlock* olderCache = currCache->older;
-                CacheBlock* newerCache = currCache->newer;
-                olderCache->newer = newerCache;
-                newerCache->older = olderCache;
-    
-                // remove from map
-                cache.erase(key);
-    
-                // free memory
-                currentItems--;
-                delete currCache;
+                currCache->value = value; // update value
+                moveToNewest(currCache);
+                return;
             }
     
-            // init a new cache with the provided values
-            CacheBlock* newCache = new CacheBlock(key, value);
-    
-            // slot into linked list
-            CacheBlock* prevNewest = rightLimit->older;
-            prevNewest->newer = newCache;
-            newCache->older = prevNewest;
-            newCache->newer = rightLimit;
-            rightLimit-> older = newCache;
-    
-            // update hashmap for key
-            cache[key] = newCache;
-            currentItems++;
-            
-            if (currentItems > capacity) {
-                // remove LRU from linked list
-                CacheBlock* prevOldest = leftLimit->newer;
-                CacheBlock* newOldest = prevOldest->newer;
-                leftLimit->newer = newOldest;
-                newOldest->older = leftLimit;
-    
-                // erase from hashmap
-                cache.erase(prevOldest->key);
-    
-                // free memory
-                currentItems--;
-                delete prevOldest;
+            // check if need evicting, then init new block 
+            if (currentItems >= capacity) {
+                CacheBlock* prevOldest = oldDummy->newer;
+                removeBlock(prevOldest);
             }
+            addNewBlock(key, value);
         }
-    };
+};
     
